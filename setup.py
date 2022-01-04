7533c733fc773b1c831277bbe67d1991c795f16d
Anders Launer Bæk-Petersen <28479232+anderslaunerbaek@users.noreply.github.com>
2022-01-04 05:32:20 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,28 @@
-from distutils.core import setup
+import setuptools
 
-setup(
-    name = 'abc-core',
-    packages = ['abc-core'],
-    version = 'version number',  # Ideally should be same as your GitHub release tag varsion
-    description = 'description',
-    author = 'Anders Launer Baek-Petersen',
-    author_email = '',
-    url = 'https://github.com/anderslaunerbaek/abc-core/',
-    download_url = 'download link you saved',
-    keywords = [],
-    classifiers = [],
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+with open("VERSION", "r", encoding="utf-8") as fh:
+    version_number = fh.read()
+
+setuptools.setup(
+    name="abc-core",
+    version=version_number,
+    author="Anders Launer Baek-Petersen",
+    author_email="anderslaunerbaek@gmail.com",
+    description="TODO",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/anderslaunerbaek/abc-core/",
+    project_urls={
+        "Bug Tracker": "https://github.com/anderslaunerbaek/abc-core//issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "abc-core"},
+    packages=setuptools.find_packages(where="abc-core"),
+    python_requires=">=3.9",
 )
