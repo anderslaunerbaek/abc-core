684f68775a04ec8dd1efd1f79b7322fed4f4809a
Anders Launer Bæk-Petersen <28479232+anderslaunerbaek@users.noreply.github.com>
2022-01-04 05:24:08 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'abc-core',
+    packages = ['abc-core'],
+    version = 'version number',  # Ideally should be same as your GitHub release tag varsion
+    description = 'description',
+    author = 'Anders Launer Baek-Petersen',
+    author_email = '',
+    url = 'https://github.com/anderslaunerbaek/abc-core/',
+    download_url = 'download link you saved',
+    keywords = [],
+    classifiers = [],
+)
